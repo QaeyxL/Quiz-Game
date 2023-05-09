bb91a75dcf7c1a04cd40710e375bc0a50fbb8322
Idle Boy <62786811+idleboy4038@users.noreply.github.com>
2023-05-09 23:26:41 +0530

    fixed

--- script.js
+++ script.js
@@ -6,29 +6,28 @@ const _result = document.getElementById("result");
 const _correctScore = document.getElementById("correct-score");
 const _totalQuestion = document.getElementById("total-question");
 
-let correctAnswer = "";
-let correctScore = 0;
-let askedCount = 0;
-const totalQuestion = 5;
+let correctAnswer = "",
+  correctScore = 0,
+  askedCount = 0,
+  totalQuestion = 5;
 
-// Load question from API
+// load question from API
 let previousIndex = -1;
 
 async function loadQuestion() {
-  const APIUrl =
-    "https://raw.githubusercontent.com/QaeyxL/Quiz-Game/main/my_questions.json";
+  const APIUrl = "/my_questions.json";
   const result = await fetch(APIUrl);
   const data = await result.json();
   _result.innerHTML = "";
   let randomIndex;
   do {
     randomIndex = Math.floor(Math.random() * data.questions.length);
-  } while (randomIndex === previousIndex);
+  } while (randomIndex == previousIndex);
   previousIndex = randomIndex;
   showQuestion(data.questions[randomIndex]);
 }
 
-// Event listeners
+// event listeners
 function eventListeners() {
   _checkBtn.addEventListener("click", checkAnswer);
   _playAgainBtn.addEventListener("click", restartQuiz);
@@ -41,24 +40,25 @@ document.addEventListener("DOMContentLoaded", function () {
   _correctScore.textContent = correctScore;
 });
 
-// Display question and options
+// display question and options
 function showQuestion(data) {
   _checkBtn.disabled = false;
   correctAnswer = data.answer;
   let optionsList = data.options.slice();
-  optionsList.splice(
-    Math.floor(Math.random() * optionsList.length),
-    0,
-    correctAnswer
-  );
   _question.innerHTML = `${data.question}`;
-  _options.innerHTML = optionsList
-    .map((option, index) => `<li>${index + 1}. <span>${option}</span></li>`)
-    .join("");
+  _options.innerHTML = `
+        ${optionsList
+          .map(
+            (option, index) => `
+            <li> ${index + 1}. <span>${option}</span> </li>
+        `
+          )
+          .join("")}
+    `;
   selectOption();
 }
 
-// Options selection
+// options selection
 function selectOption() {
   _options.querySelectorAll("li").forEach(function (option) {
     option.addEventListener("click", function () {
@@ -69,90 +69,52 @@ function selectOption() {
       option.classList.add("selected");
     });
   });
-
-  // Remove any existing duplicate of the correct answer
-  const correctOption = _options.querySelector(
-    `li span:nth-of-type(1):not(:contains("${correctAnswer}"))`
-  );
-  if (correctOption) {
-    correctOption.parentNode.remove();
-  }
-
-  // Add the correct answer if it does not exist in the options list
-  const options = _options.querySelectorAll("li");
-  const correctOptionExists = Array.from(options).some(
-    (option) => option.querySelector("span").textContent === correctAnswer
-  );
-  if (!correctOptionExists) {
-    const randomIndex = Math.floor(Math.random() * options.length);
-    const newOption = document.createElement("li");
-    newOption.innerHTML = `${
-      options.length + 1
-    }. <span>${correctAnswer}</span>`;
-    options[randomIndex].before(newOption);
-  }
 }
 
-// Answer checking
+// answer checking
 function checkAnswer() {
   _checkBtn.disabled = true;
   if (_options.querySelector(".selected")) {
-    const selectedAnswer = _options.querySelector(".selected span").textContent;
-    if (selectedAnswer === correctAnswer) {
+    let selectedAnswer = _options.querySelector(".selected span").textContent;
+    if (selectedAnswer == HTMLDecode(correctAnswer)) {
       correctScore++;
+      askedCount++;
+      _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
+    } else {
+      askedCount++;
+      _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
     }
-    askedCount++;
-    _result.innerHTML =
-      selectedAnswer === correctAnswer
-        ? `<p><i class="fas fa-check"></i>Correct Answer!</p>`
-        : `<p><i class="fas fa-times"></i>Incorrect Answer!</p><small><b>Correct Answer: </b>${correctAnswer}</small>`;
     checkCount();
   } else {
-    _result.innerHTML = `<p><i class="fas fa-question"></i>Please select an option!</p>`;
+    _result.innerHTML = `<p><i class = "fas fa-question"></i>Please select an option!</p>`;
     _checkBtn.disabled = false;
   }
 }
 
-// Check quiz count and display result
+// to convert html entities into normal text of correct answer if there is any
+function HTMLDecode(textString) {
+  let doc = new DOMParser().parseFromString(textString, "text/html");
+  return doc.documentElement.textContent;
+}
+
 function checkCount() {
   setCount();
-  if (askedCount === totalQuestion) {
-    displayResult();
+  if (askedCount == totalQuestion) {
+    _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
+    _playAgainBtn.style.display = "block";
+    _checkBtn.style.display = "none";
   } else {
-    if (_options.querySelector(".selected")) {
-      const selectedAnswer =
-        _options.querySelector(".selected span").textContent;
-      if (selectedAnswer === correctAnswer) {
-        correctScore++;
-      }
-    }
-    askedCount++;
     setTimeout(function () {
       loadQuestion();
     }, 300);
   }
 }
 
-// Set score and question count
 function setCount() {
-  _correctScore.textContent = correctScore;
   _totalQuestion.textContent = totalQuestion;
+  _correctScore.textContent = correctScore;
 }
 
-// Display quiz result
-function displayResult() {
-  if (_options.querySelector(".selected")) {
-    const selectedAnswer = _options.querySelector(".selected span").textContent;
-    if (selectedAnswer === correctAnswer) {
-      correctScore++;
-    }
-  }
-  _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
-  _playAgainBtn.style.display = "block";
-  _checkBtn.style.display = "none";
-}
-
-// Restart the quiz
 function restartQuiz() {
   correctScore = 0;
   askedCount = 0;
